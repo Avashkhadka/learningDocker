ae51ad21cc2ab3c3f663005a5fc6e47e46c6d483
Avashkhadka <avash2063@gmail.com>
2026-08-27 11:27:33 +0545

    sf

--- php/script.js
+++ php/script.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-    $('#hideBtn').click(function(){
+$(document).ready(function () {
+    $('#hideBtn').click(function () {
         $('#image').hide();
     });
-    $('#showBtn').click(function(){
+    $('#showBtn').click(function () {
         $('#image').show();
     });
 
@@ -14,8 +14,50 @@ $(document).ready(function(){
 
     // Form validation
 
+    $('#contactForm').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                email: true,
+            },
+            bookDate: {
+                required: true,
+            },
+            bookTime: {
+                required: true,
+            },
+            message: {
+                required: true,
+                minlength: 10
+            }
 
-    $("#contactForm")
+        },
+        message: {
+            name: {
+                required: "Please enter your name",
+                minlength: "Your name must consist of at least 2 characters"
+            },
+            email: {
+                required: "Please enter your email address",
+                email: "Please enter a valid email address",
+            },
+            bookDate: {
+                required: "Please select a date",
+            },
+
+            bookDate: {
+                required: "Please select a time",
+            },
+            message: {
+                required: "Please enter a message",
+                minlength: "Your message must consist of at least 10 characters"
+            }
+        }
+    })
 
 
 
